a095bfb54f024ac183eebd62f2a768d89e238f60
richy-birec <36771749+richy-birec@users.noreply.github.com>
2018-04-03 20:43:01 -0500

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,4 +1,4 @@
-#realice los suiguientes pasos para instalar la libreria de pandas## -*- coding: utf-8 -*-
+# realice los suiguientes pasos para instalar la libreria de pandas## -*- coding: utf-8 -*-
 """
 Editor de Spyder
 
